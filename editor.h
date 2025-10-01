4103f9d45a738f87b0ba2df7dff5fadc4433e661
PriscilaCaraballo <priscilatechera2@gmail.com>
2025-10-01 20:29:49 -0300

    enum

---
+++ editor.h
@@ -0,0 +1,13 @@
+#ifndef EDITOR_H
+#define EDITOR_H
+
+// ==========================================================
+// Definiciones de tipos
+// ==========================================================
+
+// Tipo de retorno de operaciones
+typedef enum _retorno {
+    OK, 
+    ERROR, 
+    NO_IMPLEMENTADA
+} TipoRet;
